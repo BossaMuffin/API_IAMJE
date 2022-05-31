6ee5bcafb2e6242def5ed3eb0d07c1859e963268
BossaMuffin <105994546+BossaMuffin@users.noreply.github.com>
2022-05-31 18:49:31 +0200

    Delete INC directory
    Useless -> using inc directory

--- INC/route_bdd.req.php
+++
@@ -1,6 +0,0 @@
-<?php
-//BDD DECLARATION + APPEL BDD CLASS------------------------  BDD + CLASS
-  require( $g_page_arbo . FOLD_CLASS . 'OBdd_connexion_local.req.php' ) ;
-  require( $g_page_arbo . FOLD_CLASS . 'OBdd_exe.req.php' ) ;
-  $BDD = new OBdd( 'iamje-1' ) ;
- 
